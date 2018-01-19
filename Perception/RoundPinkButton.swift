22a9dc85329b483d832c5bb42d315c0747344208
Gavin <baseballboyg@gmail.com>
2018-01-19 08:40:14 -0500

    End of day commit

--- Perception/RoundPinkButton.swift
+++ Perception/RoundPinkButton.swift
@@ -22,12 +22,5 @@ import UIKit
         layer.cornerRadius = layer.frame.size.height / 2
         backgroundColor = #colorLiteral(red: 0.9137254902, green: 0.2549019608, blue: 0.3411764706, alpha: 1)
     }
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
 
 }
--- Perception/VC/Signup1.swift
+++ Perception/VC/Signup1.swift
@@ -9,25 +9,59 @@
 import UIKit
 
 class Signup1: UIViewController {
-
+    
+    //IBOutlets
+    
+    @IBOutlet weak var nextBtn: RoundPinkButton!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var codeField: UITextField!
+    
+    
+    func dismissScreen(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func checkCompletionStatus(){
+        if usernameField.text != nil && emailField.text != nil && passwordField != nil && codeField.text == "Persc"{
+            nextBtn.backgroundColor = #colorLiteral(red: 0.9137254902, green: 0.2549019608, blue: 0.3411764706, alpha: 1)
+            nextBtn.alpha = CGFloat(1.0)
+            nextBtn.isEnabled = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        nextBtn.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        nextBtn.alpha = CGFloat(0.5)
+        nextBtn.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func backBtnPressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        dismissScreen()
     }
     
     @IBAction func signInBtnPressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        dismissScreen()
     }
-    
-
+    @IBAction func usernameFieldChanged(_ sender: Any) {
+        checkCompletionStatus()
+    }
+    @IBAction func emailFieldChanged(_ sender: Any) {
+        checkCompletionStatus()
+    }
+    @IBAction func passwordFieldChanged(_ sender: Any) {
+        checkCompletionStatus()
+    }
+    @IBAction func codeFieldChanged(_ sender: Any) {
+        checkCompletionStatus()
+    }
+    @IBAction func nextBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: "toSignup2", sender: nil)
+    }
 }
