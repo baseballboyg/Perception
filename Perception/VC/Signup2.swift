647a8f0c963229f99394d61609f72869b4aa1d11
gavinmccabe <baseballboyg@gmail.com>
2018-01-19 13:21:16 -0500

    End of day 2

--- Perception/VC/Signup2.swift
+++ Perception/VC/Signup2.swift
@@ -10,14 +10,27 @@ import UIKit
 
 class Signup2: UIViewController {
 
+    
+    @IBOutlet weak var codeField: UITextField!
+    @IBOutlet weak var nextBtn: RoundPinkButton!
+    
     func dismiss(){
         dismiss(animated: true, completion: nil)
     }
     
+    func checkCompletionStatus(){
+        if codeField.text == "test"{
+            nextBtn.backgroundColor = #colorLiteral(red: 0.9137254902, green: 0.2549019608, blue: 0.3411764706, alpha: 1)
+            nextBtn.alpha = CGFloat(1.0)
+            nextBtn.isEnabled = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        nextBtn.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        nextBtn.alpha = CGFloat(0.5)
+        nextBtn.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,6 +44,8 @@ class Signup2: UIViewController {
         
     }
   
+    @IBAction func nextBtnPressed(_ sender: Any) {
+    }
     
     
 }
